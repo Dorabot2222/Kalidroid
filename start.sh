4eabc889129566d0f9881c1a5f45d1f04a227dc9
Dorabot2222 <86509648+Dorabot2222@users.noreply.github.com>
2021-12-08 18:44:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,17 @@
-echo ("$Ultimate kali installer")
+#colours
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+reset='\033[0m'
+echo " "
+echo " $red
+    WELCOME TO THE ULTIMATE KALI INSTALLER"
+echo " $yellow 
+ WE HELP INSTALLING TOOLS,KALI LINUX AND ANY OTHER OS"
+pkg install git
+Pkg install wget
+
+echo $cyan INSTALLING.....
